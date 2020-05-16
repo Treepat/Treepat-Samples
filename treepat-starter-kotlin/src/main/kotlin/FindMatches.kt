38ad82396a5ab4d8b2b6d92cac8d246e06dd571a
Johan Murillo <johan_smc@hotmail.com>
2020-05-16 18:05:14 -0500

    change kotlin to 2.0.0

--- treepat-starter-kotlin/src/main/kotlin/FindMatches.kt
+++ treepat-starter-kotlin/src/main/kotlin/FindMatches.kt
@@ -1,16 +1,23 @@
 import com.github.treepat.expression.TreepatExpression
+import com.github.treepat.extensions.matchToString
 import com.github.treepat.target_tree.default_tree.DefaultTargetTree
-import com.github.treepat.target_tree.default_tree.DefaultTargetTreeNode
 
 
 fun main(args: Array<String>) {
     // Treepat Parsing
     val treepatExpression = TreepatExpression.createFromFile(args[0])
     // Tree File Parsing
-    val targetTree = DefaultTargetTree<DefaultTargetTreeNode>(args[1])
+    val targetTree = DefaultTargetTree.createFromFile(args[1])
 
-    val solutions = targetTree.findMatches(treepatExpression)
-    println(solutions.joinToString("\n-\n"))
+    val matches = targetTree.findMatches(treepatExpression)
+    val printableMatches: List<String>
+    if (matches != null) {
+        printableMatches = matches.map { it.matchToString() }
+    } else {
+        printableMatches = listOf("Match not found")
+    }
+    println(printableMatches.joinToString("\n-\n"))
+
     val hasSolutions = targetTree.hasMatch(treepatExpression)
     println(hasSolutions)
 }
